ecb66ee93127bc593a4902a64f91b15ffb738a2c
Marton <mdox@mdox.xyz>
2022-09-08 15:11:38 +0200

    Basic Routes, Layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home";
+
 export function App() {
-  return <></>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Layout>
+  );
 }
---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,5 @@
+import { PropsWithChildren } from "react";
+
+export function Layout(props: PropsWithChildren) {
+  return <>{props.children}</>;
+}
