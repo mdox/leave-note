a2f544a269cb1d65b1a7173a9ed9058aa1c8fa4b
Marton <mdox@mdox.xyz>
2022-09-09 18:11:18 +0200

    Fix Show comp

--- client/src/components/Show.tsx
+++ client/src/components/Show.tsx
@@ -1,4 +1,4 @@
-import { Children, isValidElement, ReactNode } from "react";
+import { isValidElement, ReactNode } from "react";
 
 export interface ShowProps {
   when: boolean;
@@ -8,11 +8,9 @@ export interface ShowProps {
 export function Show(props: ShowProps) {
   if (props.when) {
     if (isValidElement(props.children)) {
-      if (Children.count(props.children) === 1) {
-        return props.children;
-      } else {
-        return <>{props.children}</>;
-      }
+      return props.children;
+    } else {
+      return <>{props.children}</>;
     }
   }
 
